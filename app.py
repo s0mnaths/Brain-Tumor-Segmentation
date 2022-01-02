2b6276a706342b479c24ef165da0a69c73c3e40b
s0mnaths <somnathsharmaji05@gmail.com>
2022-01-03 01:51:48 +0530

    Update gradio app description & fix typos

--- app.py
+++ app.py
@@ -16,20 +16,15 @@ def inference(filepath):
     return plt
 
 
-
-
-
-
-#GRADIO APP
-title = "Brain MRI Tumor Detection - Sematic Segmentation using PyTorch"
-description = "Detection of tumor areas of Brain MRI images from the Kaggle dataset"
-# article = "<p style='text-align: center'><a href='https://ai.facebook.com/blog/-detectron2-a-pytorch-based-modular-object-detection-library-/' target='_blank'>Detectron2: A PyTorch-based modular object detection library</a> | <a href='https://github.com/facebookresearch/detectron2' target='_blank'>Github Repo</a></p>"
+title = "Brain MRI Tumor Detection - Semantic Segmentation using PyTorch"
+description = "Segmentation of tumor areas from Brain MRI images"
+article = "<p style='text-align: center'><a href='https://www.kaggle.com/s0mnaths/brain-mri-unet-pytorch/' target='_blank'>Kaggle Notebook: Brain MRI-UNET-PyTorch</a> | <a href='https://github.com/s0mnaths/Brain-Tumor-Segmentation' target='_blank'>Github Repo</a></p>"
 examples = [['test-samples/TCGA_CS_4942_19970222_10.png'], 
             ['test-samples/TCGA_CS_4942_19970222_11.png'], 
             ['test-samples/TCGA_CS_4942_19970222_12.png'], 
             ['test-samples/TCGA_CS_4941_19960909_15.png']]  
 
 gr.Interface(inference, inputs=gr.inputs.Image(type="filepath"), outputs=gr.outputs.Image('plot'), title=title,
-    description=description,
-    # article=article,
-    examples=examples).launch(debug=False, enable_queue=True)
+            description=description,
+            article=article,
+            examples=examples).launch(debug=False, enable_queue=True)
